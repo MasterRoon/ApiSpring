4310e4ad9c1fe5c1c8eb154b1a3878e6900c0e01
MasterRoon <joao.rosano@ufv.br>
2024-11-07 00:57:20 -0300

    segundo commit

--- src/main/java/Java/Api/ApiJava/Controle/CriarPessoaDto.java
+++ src/main/java/Java/Api/ApiJava/Controle/CriarPessoaDto.java
@@ -1,5 +1,5 @@
 package Java.Api.ApiJava.Controle;
 
-public record CriarPessoaDto(String nome,String sobrenome,Integer idade,String login,String senha) {
+public record CriarPessoaDto(Long codigoPessoa,String nome,String sobrenome,Integer idade,String login,String senha) {
 
 }
--- src/main/java/Java/Api/ApiJava/service/PessoaService.java
+++ src/main/java/Java/Api/ApiJava/service/PessoaService.java
@@ -25,7 +25,7 @@ public class PessoaService {
 
     public long createPessoa(CriarPessoaDto criarPessoaDto) {
         var entity = new Pessoa(
-                null,
+                criarPessoaDto.codigoPessoa(),//id da pessoa
                 null,
                 Instant.now(),               // creationTimestamp
                 new HashSet<>(),             // enderecos (passando um conjunto vazio, por exemplo)
